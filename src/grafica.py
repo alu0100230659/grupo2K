7af744fc9a745be803bf48cd5da28da44bb68677
Roberto Palenzuela <cpcroberto@cpcroberto-Netbook.(none)>
2013-05-13 18:33:38 +0100

    continuacion beamer

---
+++ src/grafica.py
@@ -0,0 +1,31 @@
+from math import cos
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import numpy as np
+from matplotlib.pylab import *
+
+def f(t):
+  return t**2*cos(t)
+
+t = linspace(-1, 3.5, 51)  # 51 puntos entre 0 y 3
+y = zeros(len(t))       # reserva memoria para y con elementos flotantes
+for i in xrange(len(t)):
+  y[i] = f(t[i])
+
+fig = plt.figure(1)
+ax = fig.add_subplot(111) 
+# set up axis
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+ 
+plot(t,y)
+xlabel('x')
+ylabel('y')
+legend(['f(x)=x^2*cos(x)'],'best')
+axis([-1,3.5, -10, 2])  #[tmin, tmax, ymin, ymax]
+savefig('grafica.eps')
+show()
---
+++ src/grafica2.py~
@@ -0,0 +1,33 @@
+from math import cos
+import matplotlib.pyplot as plt
+from matplotlib.ticker import MultipleLocator, FormatStrFormatter
+import numpy as np
+from matplotlib.pylab import *
+
+def f1(t):
+  return t**2*cos(t)
+
+def f2(t):
+  return -2.52*t**2+5.36*t-2.29
+
+t=linspace(-1,3.5,51)
+y1 = f1(t)
+y2 = f2(t)
+  
+fig = plt.figure(1)
+ax = fig.add_subplot(111) 
+# set up axis
+ax.spines['left'].set_position('zero')
+ax.spines['right'].set_color('none')
+ax.spines['bottom'].set_position('zero')
+ax.spines['top'].set_color('none')
+ax.xaxis.set_ticks_position('bottom')
+ax.yaxis.set_ticks_position('left')
+ 
+plot(t,y1,'r')
+hold('on')
+plot(t,y2,'b')
+legend(['f(x)=x^2*cos(x)', 'P(x)= -2.52 x^2 + 5.36 x - 2.29'],'best')
+axis([-1,3.5, -10, 2])  #[tmin, tmax, ymin, ymax]
+savefig('grafica2.eps')
+show()
